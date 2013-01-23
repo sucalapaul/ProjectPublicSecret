be4d2253160263f44a256335b9a9911a98cf9fcf
Cristian Rosu <redy_kdwa@yahoo.com>
2013-01-23 22:27:58 +0200

    circle header terminat

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  private
+  #this gives access in views to the current controller and action, so that you can display partial with conditions
+  before_filter :instantiate_controller_and_action_names
+ 
+  def instantiate_controller_and_action_names
+      @current_action = action_name
+      @current_controller = controller_name
+  end
+
   #private
   # def current_user
   #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
