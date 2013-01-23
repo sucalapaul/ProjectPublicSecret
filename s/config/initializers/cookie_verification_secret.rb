bcee13dc4a713444369029bb9e543591e98f4ba7
Paul Sucala <sucala.paul@gmail.com>
2013-01-23 22:54:37 +0200

    if likes - unlike button

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '9debd82507d881657ccacd7b339a9f3773042ec077dd55f4009c07ef42322e8387e0333f548e131ea5550933ea72ea0126864a5f29b1b34654ba9496254adb87';
