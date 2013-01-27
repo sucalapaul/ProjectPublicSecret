f6a95a4e49c82b85cea67bfbd5f7d144b96a1382
Paul Sucala <sucala.paul@gmail.com>
2013-01-27 16:22:34 +0200

    circle tabs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,10 +41,4 @@ class UsersController < ApplicationController
     end
   end
 
-	def invites
-		#@graph = Koala::Facebook::API.new(current_user.oauth_token)
-		@friend_data = current_user.facebook.get_connection("me", "friends?fields=name,picture") #@graph.get_object("me","likes")
-
-	end 
-
 end
