6159803c9a6d23d8fd0e6c4ffab738657cf256dc
Paul Sucala <sucala.paul@gmail.com>
2013-03-01 00:10:09 +0200

    production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,25 @@ ProjectPublicSecret::Application.configure do
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  # Show full error reports and disable caching
+  config.consider_all_requests_local       = true
+  config.action_controller.perform_caching = false
+
+  # Don't care if the mailer can't send
+  config.action_mailer.raise_delivery_errors = true
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.mandrillapp.com",
+    :port                 => 587,
+    :domain               => 'letsgossip.it',
+    :user_name            => 'cristianrosu',
+    :password             => 'ptMAf_wrtATrVMxtC4w9mw ',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  
+  }
+
+  config.action_mailer.default_url_options = { :host => 'letsgossip.it' }
+
   # Defaults to nil and saved in location specified by config.assets.prefix
   # config.assets.manifest = YOUR_PATH
 
