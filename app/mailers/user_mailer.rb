c25c9d59c30a0b3c1007e8805a3a80ce69986004
Paul Sucala <sucala.paul@gmail.com>
2013-03-02 22:10:42 +0200

    invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,7 @@ class UserMailer < ActionMailer::Base
 
   def signup_confirmation(user)
   	@user = user
-  	#mail to: user.email, subject: "Welcome to The Gossip", from: "The Gossip<no-reply@letsgossip.it>" 
-  	mail to: "sucalas_lab@yahoo.com", subject: "Welcome to The Gossip", from: "The Gossip<no-reply@letsgossip.it>" 
+  	mail to: user.email, subject: "Welcome to The Gossip", from: "The Gossip<no-reply@letsgossip.it>" 
   end
 
   def request_invite(email)
